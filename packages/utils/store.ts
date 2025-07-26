884ff36ad532c15a588464e5ff80635804de6d61
Brandon Tom <140762401+signalcafe@users.noreply.github.com>
2025-07-26 07:50:16 -0700

    feat(store): add init method

--- packages/utils/store.ts
+++ packages/utils/store.ts
@@ -4,26 +4,33 @@ import { create } from 'zustand'
 export interface GameState<T = Record<string, unknown>> {
   /** Arbitrary game state */
   gameData: T
+  /** Initialize gameData and remember the initial state */
+  init: (data: T) => void
   /** Merge partial data into existing gameData */
   setGameData: (data: Partial<T>) => void
-  /** Reset gameData to an empty object */
+  /** Reset gameData to the initial state */
   reset: () => void
 }
 
-const initialState: Pick<GameState<Record<string, unknown>>, 'gameData'> = {
-  gameData: {}
-}
+export const useGameStore = create<GameState<Record<string, unknown>>>(set => {
+  let initialGameData: Record<string, unknown> = {}
 
-export const useGameStore = create<GameState<Record<string, unknown>>>(set => ({
-  ...initialState,
-  setGameData: data =>
-    set(
-      produce((state: GameState<Record<string, unknown>>) => {
-        state.gameData = { ...state.gameData, ...data }
-      })
-    ),
-  reset: () =>
-    set(() => ({
-      gameData: {}
-    }))
-}))
+  return {
+    gameData: initialGameData,
+    init: data =>
+      set(() => {
+        initialGameData = { ...data }
+        return { gameData: { ...initialGameData } }
+      }),
+    setGameData: data =>
+      set(
+        produce((state: GameState<Record<string, unknown>>) => {
+          state.gameData = { ...state.gameData, ...data }
+        })
+      ),
+    reset: () =>
+      set(() => ({
+        gameData: { ...initialGameData }
+      }))
+  }
+})
